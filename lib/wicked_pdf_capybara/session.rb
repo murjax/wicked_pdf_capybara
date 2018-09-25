0d0b1bd33f8a77e94e5584b23dfebd98e1498699
Ryan Murphy <murjax@gmail.com>
2018-09-24 22:30:56 -0400

    Add tests for pdf text method

--- lib/wicked_pdf_capybara/session.rb
+++ lib/wicked_pdf_capybara/session.rb
@@ -1,12 +1,18 @@
+require 'pdf-reader'
+require 'tempfile'
 module WickedPdfCapybara
   module Capybara
     class Session
       def pdf_text
         temp_pdf = Tempfile.new('pdf')
         temp_pdf << source.force_encoding('UTF-8')
-        reader = PDF::Reader.new(temp_pdf)
-        pdf_text = reader.pages.map(&:text).first
-        pdf_text.gsub("\n", ' ').gsub("\t", ' ')
+
+        begin
+          reader = PDF::Reader.new(temp_pdf)
+          pdf_text = reader.pages.map(&:text).first
+          pdf_text.gsub("\n", ' ').gsub("\t", ' ')
+        rescue ArgumentError, PDF::Reader::MalformedPDFError
+        end
       end
     end
   end
--- spec/wicked_pdf_capybara_spec.rb
+++ spec/wicked_pdf_capybara_spec.rb
@@ -1,17 +1,27 @@
+require 'spec_helper'
+require 'wicked_pdf_capybara/session'
 RSpec.describe WickedPdfCapybara do
   it 'has a version number' do
     expect(WickedPdfCapybara::VERSION).not_to be_nil
   end
 
   describe 'Capybara::Session#pdf_text' do
+    subject { WickedPdfCapybara::Capybara::Session.new.pdf_text }
+    before(:each) do
+      expect_any_instance_of(WickedPdfCapybara::Capybara::Session).to receive_message_chain(:source, :force_encoding).and_return(source)
+    end
     context 'can be parsed by pdf reader' do
+      let(:source) { File.read('spec/encoded_pdf.txt') }
       it 'returns text stripped of newlines and tabs' do
-
+        expect(subject).to eq("Document 01  Just pretend you are a whisper floating across a mountain. Don't fiddle with it all day. Get tough with it, get strong.")
       end
     end
 
     context 'cannot be parsed by pdf reader' do
-      it { expect(subject).to be_nil }
+      let(:source) { 'foobar' }
+      it do
+        expect(subject).to be_nil
+      end
     end
   end
 end
