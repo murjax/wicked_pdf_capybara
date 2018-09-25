c7c3ba432a8877c5c12c4827d625a4a584123d22
Ryan Murphy <murjax@gmail.com>
2018-09-24 22:34:23 -0400

    Refactor pdf text logic

--- lib/wicked_pdf_capybara/session.rb
+++ lib/wicked_pdf_capybara/session.rb
@@ -4,15 +4,25 @@ module WickedPdfCapybara
   module Capybara
     class Session
       def pdf_text
-        temp_pdf = Tempfile.new('pdf')
-        temp_pdf << source.force_encoding('UTF-8')
-
         begin
-          reader = PDF::Reader.new(temp_pdf)
-          pdf_text = reader.pages.map(&:text).first
-          pdf_text.gsub("\n", ' ').gsub("\t", ' ')
+          parsed_pdf_text
         rescue ArgumentError, PDF::Reader::MalformedPDFError
         end
+      end
+
+      private
+
+      def temp_pdf
+        temp_pdf = Tempfile.new('pdf')
+        temp_pdf << source.force_encoding('UTF-8')
+      end
+
+      def pdf_reader
+        PDF::Reader.new(temp_pdf)
+      end
+
+      def parsed_pdf_text
+        pdf_reader.pages.map(&:text).first.gsub("\n", ' ').gsub("\t", ' ')
       end
     end
   end
